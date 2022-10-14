039f03003665171f175fbe757b59da0f2ebaca30
Thomas Heenan <thomas.heenan@outlook.com>
2022-10-14 20:19:35 +0100

    - menu system + square root calc.

--- Coursework/Coursework.cs
+++ Coursework/Coursework.cs
@@ -2,11 +2,34 @@
 
 namespace MiniApps
 {
-    class KeepCounting
+    class MenuSystem
     {
         public static void Main(string[] args)
-        {
-            while (true)
+        {   
+            bool menu = true;
+            bool counting = false;
+            bool rootCalc = false;
+
+            while(menu == true) 
+            {
+                Console.Clear();
+                Console.WriteLine("MiniApps - Thomas Heenan");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("1. Keep Counting");
+                Console.WriteLine("2. Square Root Calc.");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.Write("> ");
+
+                int input = Int32.Parse(Console.ReadLine());
+
+                switch(input) {
+                    case 1: counting = true; menu = false; Console.Clear(); break;
+                    case 2: rootCalc = true; menu = false; Console.Clear(); break;
+                    default: Console.Clear(); Console.Write("Please Make a Valid Selection!"); 
+                    System.Threading.Thread.Sleep(1000); continue;
+                }
+
+            while (counting == true) //1. KeepCounting
             {
 
                 // Variable declaration
@@ -64,13 +87,76 @@ namespace MiniApps
                     }
                     else
                     {
+                        Console.Clear();
                         Console.WriteLine("Try again");
                         question = 0;
                     }
 
                 }
 
+                counting = false;
+                menu = true;
                 Console.WriteLine("Finish!");
+                Console.ReadLine();
+                menu = true;
+                break;
+
+            }
+
+            while (rootCalc == true) //2. Square Root Calc.
+            {
+                int inputSqrt = 0;
+                int precision = 0;
+                bool paramMenu = true;
+
+                while(paramMenu) {
+                    Console.Write("Enter a Positive Integer: ");
+                    inputSqrt = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+                    Console.Write("Enter the decimal precision of the result: ");
+                    precision = Int32.Parse(Console.ReadLine());
+                    if(precision > 6 || precision < 0) {
+                        Console.Clear();
+                        Console.WriteLine("Please enter a valid precision!");
+                        System.Threading.Thread.Sleep(1000);
+                        continue;
+                    } else {
+                        paramMenu = false;
+                    }
+                }
+
+                double upper = inputSqrt;
+                double lower = 0;
+
+                double avrg = (upper + lower) / 2;
+
+                double avrgSqr = avrg * avrg;
+
+                Console.WriteLine(avrgSqr);
+            
+                while(avrgSqr != inputSqrt) {
+                    if (avrgSqr > inputSqrt) {
+                        upper = avrg;
+                    } else if (avrg < inputSqrt) {
+                        lower = avrg;
+                    }
+
+                    avrg = (upper + lower) / 2;
+                    avrgSqr = avrg * avrg;
+                    avrgSqr = Math.Round(avrgSqr, precision);
+
+                    Console.WriteLine(Math.Round(avrg, precision));
+
+                }
+
+                avrg = Math.Round(avrg, precision);
+                Console.WriteLine("");
+                Console.WriteLine("The square root of {0} is {1}.", inputSqrt, avrg);
+                Console.ReadLine();
+                menu = true;
+                break;
+               
+            }
 
             }
 
